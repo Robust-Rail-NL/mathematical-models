ed289c468d75bd705b1564adc783868f8052717a
firstthomas <thomas.verwaal@upcmail.nl>
2026-04-22 14:25:29 +0200

    Script changes for ls experiment for time

--- array_ls.sh
+++ array_ls.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name="rail_solver"
-#SBATCH --time=03:59:00
+#SBATCH --time=03:20:00
 #SBATCH --partition=compute
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=3968MB
 #SBATCH --account=education-eemcs-msc-cs
-#SBATCH --array=0-300
+#SBATCH --array=0-23
 
 #SBATCH --output=logs_ls_120/out_%A_%a.txt
 #SBATCH --error=logs_ls_120/err_%A_%a.txt
@@ -21,7 +21,7 @@ PROJECT_DIR=~/Robust-Rail-NL/robust-rail-solver/ServiceSiteScheduling/publish
 CONFIG_TEMPLATE=~/Robust-Rail-NL/robust-rail-solver/ServiceSiteScheduling/config_cluster.yaml
 SCENARIO_LIST=~/Robust-Rail-NL/mathematical-models/scenarios_types.txt
 
-PLAN_DIR=~/Robust-Rail-NL/mathematical-models/data_types_360/local_plans_ls_discreet
+PLAN_DIR=~/Robust-Rail-NL/mathematical-models/local_search_plans_time_20
 mkdir -p $PLAN_DIR
 
 BATCH_SIZE=5
@@ -33,7 +33,7 @@ cd $PROJECT_DIR
 START=$((SLURM_ARRAY_TASK_ID * BATCH_SIZE + 1))
 END=$((START + BATCH_SIZE - 1))
 
-RESULT_FILE=~/Robust-Rail-NL/mathematical-models/data_types_360/results_ls_discreet/results_10_${SLURM_ARRAY_TASK_ID}.csv
+RESULT_FILE=~/Robust-Rail-NL/mathematical-models/results_ls_discreet_time_20/results_${SLURM_ARRAY_TASK_ID}.csv
 echo "scenario,cost_line,time_line,plan_file" > $RESULT_FILE
 
 # ------------------ LOOP OVER SCENARIOS ------------------
